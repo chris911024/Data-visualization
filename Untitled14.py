a56e56807e9159f98fd7ef69f68ec06fa2164676
chris911024 <67829896+chris911024@users.noreply.github.com>
2020-09-25 22:34:04 +0800

    Add files via upload

---
+++ Untitled14.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+data = pd.read_csv('data.csv')
+data = data.groupby('Country/Region').sum()
+data = data.iloc[:,2:]
+
+
+def animate(i):
+    date = pd.to_datetime(data.columns[i]).strftime('%Y-%m-%d')
+    countries = list(data.iloc[:,i].sort_values()[-5:].index)
+    values = list(data.iloc[:,i].sort_values()[-5:])
+    plt.cla()
+    plt.barh(countries,values)
+    plt.title(date)
+ 
+plt.figure(figsize=(15,10))
+ani = FuncAnimation(plt.gcf(),animate,interval=1000,frames=len(data.columns))
+plt.show()
+
+
